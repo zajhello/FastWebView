1188a8d2ffb789682261a2e94e2aa3bab8aabdf1
dipper <zaihello@gmail.com>
2021-07-28 10:58:04 +0800

    SONIC AGENT框架适配修改

--- sample/src/main/java/com/tencent/sonic/demo/BrowserActivity.java
+++ sample/src/main/java/com/tencent/sonic/demo/BrowserActivity.java
@@ -17,18 +17,27 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.just.agentweb.AgentWeb;
+import com.just.agentweb.AgentWebUIControllerImplBase;
+import com.just.agentweb.IAgentWebSettings;
+import com.just.agentweb.MiddlewareWebChromeBase;
+import com.just.agentweb.MiddlewareWebClientBase;
+import com.just.agentweb.PermissionInterceptor;
+import com.just.agentweb.WebChromeClient;
 import com.tencent.sonic.R;
 import com.tencent.sonic.sdk.SonicCacheInterceptor;
 import com.tencent.sonic.sdk.SonicConfig;
@@ -48,12 +57,11 @@ import java.util.List;
 import java.util.Map;
 
 /**
- *  A demo browser activity
- *  In this demo there are three modes,
- *  sonic mode: sonic mode means webview loads html by sonic,
- *  offline mode: offline mode means webview loads html from local offline packages,
- *  default mode: default mode means webview loads html in the normal way.
- *
+ * A demo browser activity
+ * In this demo there are three modes,
+ * sonic mode: sonic mode means webview loads html by sonic,
+ * offline mode: offline mode means webview loads html from local offline packages,
+ * default mode: default mode means webview loads html in the normal way.
  */
 
 public class BrowserActivity extends Activity {
@@ -64,13 +72,20 @@ public class BrowserActivity extends Activity {
     public final static String PARAM_MODE = "param_mode";
 
     private SonicSession sonicSession;
+    private AgentWeb mAgentWeb;
+    private FrameLayout mContainer;
+    private MiddlewareWebChromeBase mMiddleWareWebChrome;
+    private MiddlewareWebClientBase mMiddleWareWebClient;
+
+    private SonicSessionClientImpl mSonicSessionClient;
+    private Intent mIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-        String url = intent.getStringExtra(PARAM_URL);
-        int mode = intent.getIntExtra(PARAM_MODE, -1);
+        mIntent = getIntent();
+        String url = mIntent.getStringExtra(PARAM_URL);
+        int mode = mIntent.getIntExtra(PARAM_MODE, -1);
         if (TextUtils.isEmpty(url) || -1 == mode) {
             finish();
             return;
@@ -83,7 +98,6 @@ public class BrowserActivity extends Activity {
             SonicEngine.createInstance(new SonicRuntimeImpl(getApplication()), new SonicConfig.Builder().build());
         }
 
-        SonicSessionClientImpl sonicSessionClient = null;
 
         // if it's sonic mode , startup sonic session at first time
         if (MainActivity.MODE_DEFAULT != mode) { // sonic mode
@@ -110,7 +124,7 @@ public class BrowserActivity extends Activity {
             // create sonic session and run sonic flow
             sonicSession = SonicEngine.getInstance().createSession(url, sessionConfigBuilder.build());
             if (null != sonicSession) {
-                sonicSession.bindClient(sonicSessionClient = new SonicSessionClientImpl());
+                sonicSession.bindClient(mSonicSessionClient = new SonicSessionClientImpl());
             } else {
                 // this only happen when a same sonic session is already running,
                 // u can comment following codes to feedback as a default mode.
@@ -123,6 +137,9 @@ public class BrowserActivity extends Activity {
         // in the real world, the init flow may cost a long time as startup
         // runtime、init configs....
         setContentView(R.layout.activity_browser);
+        mContainer = this.findViewById(R.id.container);
+        initAgentWeb();
+
 
         FloatingActionButton btnFab = (FloatingActionButton) findViewById(R.id.btn_refresh);
         btnFab.setOnClickListener(new View.OnClickListener() {
@@ -135,8 +152,126 @@ public class BrowserActivity extends Activity {
         });
 
         // init webview
-        WebView webView = (WebView) findViewById(R.id.webview);
-        webView.setWebViewClient(new WebViewClient() {
+//        WebView webView = (WebView) findViewById(R.id.webview);
+//        WebSettings webSettings = webView.getSettings();
+//
+//        // add java script interface
+//        // note:if api level lower than 17(android 4.2), addJavascriptInterface has security
+//        // issue, please use x5 or see https://developer.android.com/reference/android/webkit/
+//        // WebView.html#addJavascriptInterface(java.lang.Object, java.lang.String)
+//        webSettings.setJavaScriptEnabled(true);
+//        webView.removeJavascriptInterface("searchBoxJavaBridge_");
+//        intent.putExtra(SonicJavaScriptInterface.PARAM_LOAD_URL_TIME, System.currentTimeMillis());
+//        webView.addJavascriptInterface(new SonicJavaScriptInterface(sonicSessionClient, intent), "sonic");
+//
+//        // init webview settings
+//        webSettings.setAllowContentAccess(true);
+//        webSettings.setDatabaseEnabled(true);
+//        webSettings.setDomStorageEnabled(true);
+//        webSettings.setAppCacheEnabled(true);
+//        webSettings.setSavePassword(false);
+//        webSettings.setSaveFormData(false);
+//        webSettings.setUseWideViewPort(true);
+//        webSettings.setLoadWithOverviewMode(true);
+
+
+        // webview is ready now, just tell session client to bind
+        if (mSonicSessionClient != null) {
+            mSonicSessionClient.bindWebView(mAgentWeb.getWebCreator().getWebView());
+            mSonicSessionClient.clientReady();
+        } else { // default mode
+            mAgentWeb.getWebCreator().getWebView().loadUrl(url);
+        }
+    }
+
+    private void initAgentWeb() {
+        mAgentWeb = AgentWeb.with(this)
+                .setAgentWebParent(mContainer, new FrameLayout.LayoutParams(-1, -1))
+                .useDefaultIndicator(getIndicatorColor(), getIndicatorHeight())
+                .setWebChromeClient(getWebChromeClient())
+                .setWebViewClient(getWebViewClient())
+//                .setWebView(getWebView())
+                .setPermissionInterceptor(getPermissionInterceptor())
+//                .setWebLayout(getWebLayout())
+                .setAgentWebUIController(getAgentWebUIController())
+                .interceptUnkownUrl()
+//                .setOpenOtherPageWays(getOpenOtherAppWay())
+                .useMiddlewareWebChrome(getMiddleWareWebChrome())
+                .useMiddlewareWebClient(getMiddleWareWebClient())
+                .setAgentWebWebSettings(getAgentWebSettings())
+//                .setMainFrameErrorView(mErrorLayoutEntity.layoutRes, mErrorLayoutEntity.reloadId)
+                .setSecurityType(AgentWeb.SecurityType.STRICT_CHECK)
+                .createAgentWeb()
+                .ready()
+                .get();
+    }
+
+    protected @NonNull
+    MiddlewareWebChromeBase getMiddleWareWebChrome() {
+        return this.mMiddleWareWebChrome = new MiddlewareWebChromeBase() {
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+//                setTitle(view, title);
+            }
+        };
+    }
+
+    public @Nullable
+    IAgentWebSettings getAgentWebSettings() {
+        return new SonicWebSettingImpl(this, mIntent, mSonicSessionClient);
+    }
+
+    protected @NonNull
+    MiddlewareWebClientBase getMiddleWareWebClient() {
+        return this.mMiddleWareWebClient = new MiddlewareWebClientBase() {
+        };
+    }
+
+//    public @Nullable
+//    DefaultWebClient.OpenOtherPageWays getOpenOtherAppWay() {
+//        return null;
+//    }
+
+
+    public @Nullable
+    AgentWebUIControllerImplBase getAgentWebUIController() {
+        return null;
+    }
+
+//    public @Nullable
+//    IWebLayout getWebLayout() {
+//        return null;
+//    }
+
+    public @Nullable
+    PermissionInterceptor getPermissionInterceptor() {
+        return null;
+    }
+
+
+    public int getIndicatorColor() {
+        return Color.parseColor("#ff0000");
+    }
+
+    public int getIndicatorHeight() {
+        return 3;
+    }
+
+    public @Nullable
+    WebChromeClient getWebChromeClient() {
+        return null;
+    }
+
+    protected @Nullable
+    WebView getWebView() {
+        return null;
+    }
+
+
+    public @Nullable
+    com.just.agentweb.WebViewClient getWebViewClient() {
+        return new com.just.agentweb.WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
@@ -156,39 +291,9 @@ public class BrowserActivity extends Activity {
                 if (sonicSession != null) {
                     return (WebResourceResponse) sonicSession.getSessionClient().requestResource(url);
                 }
-                return null;
+                return super.shouldInterceptRequest(view, url);
             }
-        });
-
-        WebSettings webSettings = webView.getSettings();
-
-        // add java script interface
-        // note:if api level lower than 17(android 4.2), addJavascriptInterface has security
-        // issue, please use x5 or see https://developer.android.com/reference/android/webkit/
-        // WebView.html#addJavascriptInterface(java.lang.Object, java.lang.String)
-        webSettings.setJavaScriptEnabled(true);
-        webView.removeJavascriptInterface("searchBoxJavaBridge_");
-        intent.putExtra(SonicJavaScriptInterface.PARAM_LOAD_URL_TIME, System.currentTimeMillis());
-        webView.addJavascriptInterface(new SonicJavaScriptInterface(sonicSessionClient, intent), "sonic");
-
-        // init webview settings
-        webSettings.setAllowContentAccess(true);
-        webSettings.setDatabaseEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setAppCacheEnabled(true);
-        webSettings.setSavePassword(false);
-        webSettings.setSaveFormData(false);
-        webSettings.setUseWideViewPort(true);
-        webSettings.setLoadWithOverviewMode(true);
-
-
-        // webview is ready now, just tell session client to bind
-        if (sonicSessionClient != null) {
-            sonicSessionClient.bindWebView(webView);
-            sonicSessionClient.clientReady();
-        } else { // default mode
-            webView.loadUrl(url);
-        }
+        };
     }
 
     @Override
---
+++ sample/src/main/java/com/tencent/sonic/demo/SonicWebSettingImpl.java
@@ -0,0 +1,68 @@
+package com.tencent.sonic.demo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.webkit.DownloadListener;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.just.agentweb.AbsAgentWebSettings;
+import com.just.agentweb.AgentWeb;
+import com.just.agentweb.DefaultDownloadImpl;
+import com.just.agentweb.IAgentWebSettings;
+import com.just.agentweb.WebListenerManager;
+
+public class SonicWebSettingImpl extends AbsAgentWebSettings {
+
+    private SonicSessionClientImpl mSonicSessionClient;
+    protected AgentWeb mAgentWeb;
+    private Activity mActivity;
+    private Intent mIntent;
+
+    public SonicWebSettingImpl(Activity activity, Intent intent, SonicSessionClientImpl mSonicSessionClient) {
+        super();
+        this.mActivity = activity;
+        this.mIntent = intent;
+        this.mSonicSessionClient = mSonicSessionClient;
+    }
+
+    @Override
+    protected void bindAgentWebSupport(AgentWeb agentWeb) {
+        this.mAgentWeb = agentWeb;
+    }
+
+
+    @Override
+    public IAgentWebSettings toSetting(WebView webView) {
+        super.toSetting(webView);
+        WebSettings webSettings = webView.getSettings();
+
+        // add java script interface
+        // note:if api level lower than 17(android 4.2), addJavascriptInterface has security
+        // issue, please use x5 or see https://developer.android.com/reference/android/webkit/
+        // WebView.html#addJavascriptInterface(java.lang.Object, java.lang.String)
+        webSettings.setJavaScriptEnabled(true);
+        webView.removeJavascriptInterface("searchBoxJavaBridge_");
+        mIntent.putExtra(SonicJavaScriptInterface.PARAM_LOAD_URL_TIME, System.currentTimeMillis());
+        webView.addJavascriptInterface(new SonicJavaScriptInterface(mSonicSessionClient, mIntent), "sonic");
+
+        // init webview settings
+        webSettings.setAllowContentAccess(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setSavePassword(false);
+        webSettings.setSaveFormData(false);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+        return this;
+    }
+
+
+    @Override
+    public WebListenerManager setDownloader(WebView webView, DownloadListener downloadListener) {
+        return super.setDownloader(webView,
+                DefaultDownloadImpl.create(this.mActivity
+                        , webView, mAgentWeb.getPermissionInterceptor()));
+    }
+}
